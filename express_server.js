838567bb83324827ec167994f4fe88d74a876bef
zhanjerr <jerry.h.zhan@gmail.com>
2017-04-11 23:48:14 +0000

    added a header partial

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-let urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
