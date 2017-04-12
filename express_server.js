d78253bede8b3792a8870106ef8826fdd91af033
zhanjerr <jerry.h.zhan@gmail.com>
2017-04-12 05:02:00 +0000

    added 'add new url' page + generate random string function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+
+
+const generateRandomString(length) => {
+  let string = '';
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++){
+    string += possible.charAt(Math.floor(Math.random()*(possible.length - 1)));
+  }
+  console.log(string);
+}
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,6 +27,10 @@ app.get("/", (req, res) => {
   res.end();
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 // app.get("/hello", (req, res) => {
 //   res.end("<html><body>Hello <b>World</b></body></html>\n");
 // });
@@ -24,6 +40,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 });
 
+app.post("/urls", (req, res) =>{
+  console.log(req.body);
+  res.send("ok");
+});
 
 app.get("/urls/:id", (req, res) => {
 let templateVars = { shortURL: req.params.id,
