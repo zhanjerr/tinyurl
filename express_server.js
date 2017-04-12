336794b5e4f442b32c1feb8c8382e0570a1f5ab9
zhanjerr <jerry.h.zhan@gmail.com>
2017-04-12 20:19:48 +0000

    documented all the functions, and got new update form parsing the correct data

--- express_server.js
+++ express_server.js
@@ -3,7 +3,6 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 
-
 const generateRandomString = (length) => {
   let string = '';
   let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -21,51 +20,73 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//home page
 app.get("/", (req, res) => {
   // debugger;
   res.send("Hello!");
   res.end();
 });
 
+//displaying page of all shortURL : longURL
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
 });
 
+//delivering url obj in JSON
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//displaying add new url page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//add a new short URL given long URL
 app.post("/urls/new", (req, res) =>{
   const shortURL = generateRandomString(6);
   urlDatabase[shortURL] = `http://${req.body.longURL}`;
   res.redirect(`/urls/${shortURL}`);
 });
 
+//displaying invidual shortURL : longURL pairs given shortURL
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
                      longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+//deleting entry
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
 
+//updating longURL given shortURL and new longURL
+app.post("/urls/:id/update", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  res.redirect("/urls");
+});
+
+//redirecting to longURL given shortURL
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+//catch all for all get requests
 app.get('/*', (req, res) => {
   res.status(404);
   res.send(`404 page not found: ${req.method} ${req.url} is not valid`)
-})
+});
+
+//catch all for all post requests
+app.post('/*', (req, res) => {
+  res.status(404);
+  res.send(`404 page not found: ${req.method} ${req.url} is not valid`)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on PORT: ${PORT}`);
